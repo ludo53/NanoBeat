5c920d46d56a5d4100edb72472e16d041b8284eb
Ludo <97764715+ludo53@users.noreply.github.com>
2025-08-02 22:02:27 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/nanobeat/main.go
@@ -0,0 +1,23 @@
+// cmd/nanobeat/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"nanobeat/internal/nanobeat"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := nanobeat.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
